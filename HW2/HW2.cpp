9354dfd5ed8b2c31ad4cf402b6ab71e4ea3bc5a8
amanda.salazar <amanda.salazar@meteologica.com>
2020-01-29 11:12:52 +0100

    Nueva rama compatible con Eclipse.

---
+++ HW2/HW2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#include "graph.h"
+
+using namespace std;
+
+int main() {
+
+    graph grafo(5, 1);
+
+    grafo.print_graph();
+
+    grafo.set_node_value(1, 12);
+
+    cout << endl << grafo.E() << " " << grafo.get_node_value(1);
+    return 0;
+}
--- HW2/graph.cpp
+++ HW2/graph.cpp
@@ -6,51 +6,108 @@
  */
 
 #include "graph.h"
-#include<iostream>
+#include <iostream>
+#include <vector>
 
-graph::graph(const int &size, const float &density, const int &lower_range,
-		const int &upper_range) :
-		size(size), density(density), lower_range(lower_range), upper_range(
-				upper_range) {
+using namespace std;
 
-	// TODO: tiene sentido declarar esto aqui?
-	srand(time(0));
+graph::graph(const int &size, const float &density, const pairs &range) : size(size), density(density), range(range) {
 
-	ad_matrix = new int*[size];
-	for (int i = 0; i < size; ++i) {
-		ad_matrix[i] = new int[size];
-	}
+    // TODO: tiene sentido declarar esto aqui?
+    srand(time(0));
 
-	for (int i = 0; i < size; ++i) {
-		for (int j = i; j < size; ++j) {
-			if (i == j || prob() >= density)
-				ad_matrix[i][j] = 0;
-			else {
-				ad_matrix[i][j] = ad_matrix[j][i] = prob(lower_range, upper_range);
-			}
-		}
-	}
+    ad_matrix = new int *[size];
 
+    for (int i = 0; i < size; ++i) {
+        nodes_value.push_back(i + 1);
+        ad_matrix[i] = new int[size];
+        for (int j = 0; j < i; ++j) {
+            if (prob() < density) {
+                add_edge(i, j);
+            }
+        }
+    }
 }
 
 float graph::prob() {
-	return rand() / static_cast<float>(RAND_MAX);
+    return rand() / static_cast<float>(RAND_MAX);
 }
 
-int graph::prob(const int &lower_range, const int &upper_range) {
-	int total_range = upper_range - lower_range;
-	return (rand() % total_range) + lower_range;
+int graph::prob(const pairs &range) {
+    int total_range = range.second - range.first + 1;
+    return (rand() % total_range) + range.first;
+}
 
+int graph::V() {
+    return size;
+}
+int graph::E() {
+
+    int edges = 0;
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < i; ++j) {
+            if (adjacent(i, j))
+                edges++;
+        }
+    }
+
+    return edges;
+}
+
+bool graph::adjacent(int x, int y) {
+    return ad_matrix[x][y] != 0;
+}
+
+std::vector<int> graph::neighbors(int x) {
+    std::vector<int> adjacents;
+    for (int i = 0; i < size; ++i) {
+        if (adjacent(x, i))
+            adjacents.push_back(i);
+    }
+    return adjacents;
+}
+
+void graph::add_edge(int x, int y) {
+    add_edge(x, y, prob(range));
+}
+
+void graph::add_edge(int x, int y, int distance) {
+    if (!adjacent(x, y) && x != y && distance <= range.second && distance >= range.first) {
+        ad_matrix[x][y] = distance;
+        ad_matrix[y][x] = distance;
+    }
+}
+
+void graph::delete_edge(int x, int y) {
+    if (adjacent(x, y)) {
+        ad_matrix[x][y] = 0;
+        ad_matrix[y][x] = 0;
+    }
+}
+
+int graph::get_node_value(int x) {
+    if (x < size)
+        return nodes_value.at(x);
+    return -1;  // returns -1 if x is not a node
+}
+
+void graph::set_node_value(int x, int a) {
+    if (a >= 0)
+        nodes_value.at(x) = a;
+}
+
+int graph::get_edge_value(int x, int y) {
+    if (adjacent(x, y))
+        return ad_matrix[x][y];
+    return -1;  // returns -1 if there is no edge
 }
 
 void graph::print_graph() {
 
-	for (int i = 0; i < size; ++i) {
-
-		for (int j = 0; j < size; ++j) {
-			std::cout << ad_matrix[i][j];
-		}
-		std::cout << std::endl;
-
-	}
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < size; ++j) {
+            cout << ad_matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
 }
