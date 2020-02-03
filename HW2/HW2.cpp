d3b1890cef6450c785f16ea298f820ce0cf0d740
amanda.salazar <amanda.salazar@meteologica.com>
2020-02-03 14:07:03 +0100

    Ultimos retoces antes de los destructores.

--- HW2/HW2.cpp
+++ HW2/HW2.cpp
@@ -1,7 +1,6 @@
 #include<iostream>
 #include <numeric>
 
-
 #include "graph.h"
 #include "priority_queue.h"
 #include "shortest_path.h"
@@ -10,27 +9,45 @@ using namespace std;
 
 int main() {
 
-	srand(123);
+	srand(time(0));
 
-	int nodes_no = 5;
-	graph G(nodes_no , 0.2);
+	int nodes_no = 50;
 
-	G.print_graph();
+	for (auto &density : vector<float> { 0.2, 0.4 }) {
 
-	ShortestPath djisktra(G);
+		cout << endl << "Computing average shortest path for a random graph of "
+				<< nodes_no << " and density " << density * 100 << "% ..."
+				<< endl;
+		graph G(nodes_no, density);
 
-	vector<int> paths;
+		G.print_graph();
 
-	for (int i = 1; i < nodes_no ; ++i) {
-		int lon = djisktra.path_size(0, i);
-		if(lon>=1){
-			paths.push_back(lon);
-			cout << i << " "<< lon << endl;
+		ShortestPath djisktra(G);
+
+		vector<int> paths;
+
+		for (int i = 1; i < nodes_no; ++i) {
+			int lon = djisktra.path_size(0, i);
+			if (lon >= 1) {
+				paths.push_back(lon);
+
+				vector<int> longest_path = djisktra.path(0, i);
+
+				for (auto &node : longest_path) {
+					cout << node << "--";
+				}
+				cout << " | " << lon;
+
+			}
+			cout << endl;
 		}
+
+		cout << endl << "Average path length: "
+				<< accumulate(paths.begin(), paths.end(), 0.0) / paths.size()
+				<< endl << endl
+				<< "------------------------------------------------------";
+
 	}
-
-	cout << endl << accumulate( paths.begin(), paths.end(), 0.0)/paths.size() <<endl;
-
 
 	return 0;
 }
--- HW2/shortest_path.cpp
+++ HW2/shortest_path.cpp
@@ -15,7 +15,7 @@
 
 ShortestPath::ShortestPath(const graph &G) :
 		G(G) {
-	int size =this->G.V();
+	int size = this->G.V();
 	nodes_queues.resize(size, size);
 }
 
@@ -28,39 +28,52 @@ std::vector<int> ShortestPath::vertices() {
 
 std::vector<int> ShortestPath::path(const int &u, const int &w) {
 
-	PriorityQueue closed_queue = nodes_queues.at(u);
-	std::vector<int> shortest_path = {w};
+	std::vector<int> shortest_path = { w };
 
 	int min_path = path_size(u, w);
+	PriorityQueue closed_queue = nodes_queues.at(u);
 
 	int center = w;
-	if(min_path>0){
-	while(center!=u){
+	if (min_path > 0) {
+
+		while (center != u) {
+
 			std::vector<int> adjacents = G.neighbors(center);
-			std::vector<int> priorities;
-			for (auto &ad : adjacents) {
-				if(closed_queue.contains_node(ad)){
-					priorities.push_back(closed_queue.get_priority(ad));
+			auto next_node = adjacents.begin();
+
+			while (next_node != adjacents.end() && *next_node != center) {
+				if (!closed_queue.contains_node(*next_node)) {
+					++next_node;
+				} else {
+					int nex_node_distance = closed_queue.get_priority(
+							*next_node) + G.get_edge_value(center, *next_node);
+					int center_priority = closed_queue.get_priority(center);
+					if (center_priority == nex_node_distance
+							&& center_priority >= 0) {
+						center = *next_node;
+						shortest_path.push_back(center);
+					} else {
+						++next_node;
+					}
 				}
 			}
-
-			int min_pos = std::distance(priorities.begin(),
-						std::min_element(priorities.begin(), priorities.end()));
-
-				center =adjacents[min_pos];
-				shortest_path.push_back(center);
+			if (next_node == adjacents.end()) {
+				std::cout << "Reached end of list." << std::endl;
+				return shortest_path;
+			}
 		}
+
 	}
 
 	return shortest_path;
-
 }
+
 
 int ShortestPath::path_size(const int &u, const int &w) {
 
 	PriorityQueue closed_queue = nodes_queues.at(u);
 
-	if(!closed_queue.contains_node(w))
+	if (!closed_queue.contains_node(w))
 		djistra_algo(u, w);
 
 	return nodes_queues.at(u).get_priority(w); // returns -1 if the nodes are not connected
@@ -74,7 +87,6 @@ void ShortestPath::djistra_algo(const int &u, const int &w) {
 
 	if (closed_queue->size() == 0)
 		closed_queue->insert(u, 0);
-
 
 	int center = closed_queue->tail();
 
@@ -105,6 +117,8 @@ void ShortestPath::djistra_algo(const int &u, const int &w) {
 		open_queue.minPrioirty();
 
 	}
+
+	closed_queue->priority_sort();
 
 }
 
